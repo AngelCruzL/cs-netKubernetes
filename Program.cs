e83efb9d95e30798cc8bd95be8af378de989ed0f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-05 12:10:34 -0600

    chore: set up services and mappers

--- Program.cs
+++ Program.cs
@@ -1,13 +1,68 @@
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Internal;
+using Microsoft.IdentityModel.Tokens;
+using NetKubernetes.Data;
+using NetKubernetes.Data.Estates;
+using NetKubernetes.Data.Users;
 using NetKubernetes.Middleware;
+using NetKubernetes.Models;
+using NetKubernetes.Profiles;
+using NetKubernetes.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+  opt.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+    .EnableSensitiveDataLogging();
+  opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
+});
+
+builder.Services.AddScoped<IEstateRepository, EstateRepository>();
+
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+  var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+  opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new EstateProfile()); });
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var builderSecurity = builder.Services.AddIdentityCore<User>();
+var identityBuilder = new IdentityBuilder(builderSecurity.UserType, builderSecurity.Services);
+identityBuilder.AddEntityFrameworkStores<AppDbContext>();
+identityBuilder.AddSignInManager<SignInManager<User>>();
+builder.Services.AddSingleton<ISystemClock, SystemClock>();
+builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+builder.Services.AddScoped<IUserSession, UserSession>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+  opt.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = key,
+    ValidateIssuer = false,
+    ValidateAudience = false
+  };
+});
 
 var app = builder.Build();
 
