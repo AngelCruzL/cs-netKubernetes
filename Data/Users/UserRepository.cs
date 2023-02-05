d78848cfadd3fb5e7daebb8180c0492cae822701
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-05 08:07:39 -0600

    feat: implement middleware on UserRepository.cs file

--- Data/Users/UserRepository.cs
+++ Data/Users/UserRepository.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using NetKubernetes.Dtos.UserDtos;
+using NetKubernetes.Middleware;
 using NetKubernetes.Models;
 using NetKubernetes.Token;
 
@@ -45,11 +48,24 @@ public class UserRepository : IUserRepository
   public async Task<UserResponseDto> GetUser()
   {
     var user = await _userManager.FindByNameAsync(_userSession.GetUserSession());
+    if (user is null)
+      throw new MiddlewareException(HttpStatusCode.Unauthorized, new { message = "User not exists on database" });
+
     return TransformerUserToUserDto(user!);
   }
 
   public async Task<UserResponseDto> RegisterUser(UserRegisterRequestDto userRegisterRequestDto)
   {
+    var emailExists = await _context.Users.Where(u => u.Email == userRegisterRequestDto.Email).AnyAsync();
+    if (emailExists)
+      throw new MiddlewareException(HttpStatusCode.BadRequest,
+        new { message = "The User Email already exists on database" });
+
+    var userNameExists = await _context.Users.Where(u => u.UserName == userRegisterRequestDto.UserName).AnyAsync();
+    if (userNameExists)
+      throw new MiddlewareException(HttpStatusCode.BadRequest,
+        new { message = "The User UserName already exists on database" });
+
     var user = new User
     {
       Name = userRegisterRequestDto.Name,
@@ -59,15 +75,22 @@ public class UserRepository : IUserRepository
       PhoneNumber = userRegisterRequestDto.PhoneNumber
     };
 
-    await _userManager.CreateAsync(user, userRegisterRequestDto.Password!);
+    var result = await _userManager.CreateAsync(user, userRegisterRequestDto.Password!);
+    if (result.Succeeded) return TransformerUserToUserDto(user);
 
-    return TransformerUserToUserDto(user);
+    throw new Exception("Error on register user");
   }
 
   public async Task<UserResponseDto> LoginUser(UserLoginRequestDto userLoginRequestDto)
   {
     var user = await _userManager.FindByEmailAsync(userLoginRequestDto.Email!);
-    await _signInManager.CheckPasswordSignInAsync(user!, userLoginRequestDto.Password!, false);
-    return TransformerUserToUserDto(user!);
+    if (user is null)
+      throw new MiddlewareException(HttpStatusCode.Unauthorized,
+        new { message = "The User Email not exists on database" });
+
+    var result = await _signInManager.CheckPasswordSignInAsync(user, userLoginRequestDto.Password!, false);
+    if (result.Succeeded) return TransformerUserToUserDto(user);
+
+    throw new MiddlewareException(HttpStatusCode.Unauthorized, new { message = "The User Password is incorrect" });
   }
 }
